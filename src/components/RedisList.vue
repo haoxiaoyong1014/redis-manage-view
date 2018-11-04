f9c1c7ec7345950c82c0cce12fd3e4aa856f44de
haoxiaoyong <hxyHelloWorld@163.com>
2018-11-04 22:18:18 +0800

    add page

--- src/components/RedisList.vue
+++ src/components/RedisList.vue
@@ -1,83 +1,98 @@
 <template>
-    <div class="layout">
-      <Layout>
-        <Header style="top: 0px">
-          <Menu mode="horizontal" theme="dark" active-name="1">
-            <div class="layout-nav">
-              <MenuItem name="1">
-                <Icon type="ios-navigate"></Icon>
-                Manage redis server
-              </MenuItem>
-            </div>
-          </Menu>
-        </Header>
-        </Layout>
-      <Table highlight-row ref="currentRowTable" :columns="columns3" :data="data1"></Table>
-      <Button @click="handleClearCurrentRow">Clear</Button>
-    </div>
+  <div class="layout">
+    <Layout>
+      <Header style="top: 0px">
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-nav">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>
+              Manage redis server
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+    </Layout>
+    <Table highlight-row ref="currentRowTable" :columns="columns3" :data="data1"></Table>
+    <Page :total=total @on-change="change"></Page>
+  </div>
 </template>
 <script>
-import axios from 'axios'
-import 'axios/dist/axios.js'
-    export default {
-        name: "redis-list",
-      data () {
-        return {
-          columns3: [
-            {
-              type: 'index',
-              width: 60,
-              align: 'center'
-            },
-            {
-              title: 'key',
-             /* key: 'name',*/
-              align: 'center'
-            },
-            {
-              title: '操作',
-              key: 'actor',
-              align: 'center'
-            }
-          ],
-          data1: [
+ /* https://blog.csdn.net/u013144287/article/details/78879044*/
+  import axios from 'axios'
+  import 'axios/dist/axios.js'
 
-          ]
-
-        }
-
-      },
-      methods:{
-          getList(){
-            axios.post('http://localhost:8087/redis/keys', {"pageNow":1,"pageSize":10},{emulateJSON: true})
-              .then(function (res) {
-                console.log(res.data.content)
-                var arr_model=res.data.content;
-               for(var i=0;i<arr_model.length;i++){
-                 this.data1.push({
-                   name:arr_model[i].name,
-                 })
-               }
-              })
+  export default {
+    name: "redis-list",
+    data() {
+      return {
+        columns3: [
+          {
+            type: 'index',
+            width: 60,
+            align: 'center'
           },
-        handleClearCurrentRow () {
-          this.$refs.currentRowTable.clearCurrentRow();
-        }
+          {
+            title: 'key',
+            key: 'name',
+            align: 'center'
+          },
+          {
+            title: '操作',
+            key: 'actor',
+            align: 'center'
+          }
+        ],
+        data1: [],
+        total: 2,
+        pageSize: 10
+      }
+    },
+    methods: {
+      getList() {
+        axios.post('http://localhost:8087/redis/keys', {"pageNow": 1, "pageSize": this.pageSize}, {emulateJSON: true})
+          .then(res => {
+            var arr_model = res.data.content.name;
+            this.total = res.data.content.total
+            for (var i = 0; i < arr_model.length; i++) {
+              this.data1.push({
+                name: arr_model[i]
+              })
+            }
+          })
       },
-      created :function () {
-        this.getList()
-        }
+      change: function (page) {
+        axios.post('http://localhost:8087/redis/keys', {
+          "pageNow": page,
+          "pageSize": this.pageSize
+        }, {emulateJSON: true})
+          .then(res => {
+            var arr_mode = res.data.content.name;
+            console.log(arr_mode)
+            this.total = res.data.content.total;
+            for (var i = 0; i < arr_mode.length; i++) {
+              this.data1[i].name=
+                 arr_mode[i]
+
+            }
+          })
+        console.log(page)
+      }
+    },
+    created: function () {
+      this.getList()
     }
+  }
 </script>
 
 <style scoped>
-  .layout-nav{
+  .layout-nav {
     width: auto;
     margin: 0 auto;
     margin-right: auto;
     font-size: 1.875em;
   }
-  .layout{
+
+  .layout {
     border: 1px solid #d7dde4;
     background: #f5f7f9;
     position: relative;
